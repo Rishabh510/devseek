b2ad2e2014d1ab8a301af1b3793c669800d678eb
Rishabh Raizada <rishabh5102000@gmail.com>
2020-10-15 17:53:32 +0530

    lesson_1 complete

---
+++ lib/views/homeView.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Container(
+                  child: Image.asset('images/logo.png'),
+                ),
+                Text(
+                  'For the developers who seek',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            RaisedButton(
+              color: Colors.blue.withOpacity(0.5),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+              padding: EdgeInsets.symmetric(horizontal: 64, vertical: 16),
+              onPressed: () {},
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
