0cd216fbc3ddc517fb3d07056dc75b414b9952d9
Rishabh Raizada <rishabh5102000@gmail.com>
2020-10-29 16:18:20 +0530

    lesson_5 complete

--- lib/views/notificationsView.dart
+++ lib/views/notificationsView.dart
@@ -3,6 +3,30 @@ import 'package:flutter/material.dart';
 class NotificationsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notifications'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        padding: EdgeInsets.all(8),
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              border: Border.symmetric(horizontal: BorderSide()),
+              gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [
+                Colors.orange,
+                Colors.yellow,
+              ]),
+            ),
+            child: ListTile(
+              leading: CircleAvatar(),
+              title: Text('bla bal abla has started following you'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/views/postView.dart
+++ lib/views/postView.dart
@@ -3,6 +3,50 @@ import 'package:flutter/material.dart';
 class PostView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('New Post'),
+        actions: [
+          IconButton(icon: Icon(Icons.send), onPressed: () {}),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 250,
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(icon: Icon(Icons.add), onPressed: () {}),
+                      Text('Upload a photo'),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Description',
+                  border: OutlineInputBorder(),
+                ),
+                maxLines: 5,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
