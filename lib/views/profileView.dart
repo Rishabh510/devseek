20d5b8f8b8b0ca13cba688090bf4f1dee644b6c5
Rishabh Raizada <rishabh5102000@gmail.com>
2020-10-29 17:29:59 +0530

    lesson_6 complete

--- lib/views/profileView.dart
+++ lib/views/profileView.dart
@@ -1,8 +1,121 @@
+import 'dart:ui';
+
+import 'package:devseek/constants.dart';
 import 'package:flutter/material.dart';
 
-class ProfileView extends StatelessWidget {
+class ProfileView extends StatefulWidget {
+  @override
+  _ProfileViewState createState() => _ProfileViewState();
+}
+
+class _ProfileViewState extends State<ProfileView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Profile'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: blue,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 24,
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    '2',
+                                    style: TextStyle(fontSize: 24),
+                                  ),
+                                  Text('Posts'),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    '2',
+                                    style: TextStyle(fontSize: 24),
+                                  ),
+                                  Text('Followers'),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    '2',
+                                    style: TextStyle(fontSize: 24),
+                                  ),
+                                  Text('Following'),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        OutlineButton(
+                          onPressed: () {},
+                          child: Text('Edit Profile'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('DevSeek ID'),
+                    Text(
+                      'DevSeek User Name',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    Text(
+                      'DevSeek Bio',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
