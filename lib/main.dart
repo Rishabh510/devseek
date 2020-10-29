f565bf4e3aefd0190d88ce36388f6d8eb5d83b8c
Rishabh Raizada <rishabh5102000@gmail.com>
2020-10-29 17:59:16 +0530

    lesson_8 complete

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: EditProfileView(),
+      home: TabDeciderView(),
     );
   }
 }
