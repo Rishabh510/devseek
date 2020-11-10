996f9dcb793773bc155ab7102a4095982f8f7dec
Rishabh Raizada <rishabh5102000@gmail.com>
2020-11-10 18:53:43 +0530

    lesson_10 complete

--- lib/views/homeView.dart
+++ lib/views/homeView.dart
@@ -1,9 +1,25 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:http/http.dart' as http;
 import '../constants.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  static const url = 'https://api.quotable.io/random';
+
+  Future<void> getRandomQuote() async {
+    final response = await http.get(url);
+    print(response.body);
+    // return jsonDecode(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,13 +35,19 @@ class HomeView extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            OutlineButton(
+              onPressed: () {
+                getRandomQuote();
+              },
+              child: Text('Test'),
+            ),
             Card(
               color: Colors.yellow,
               child: Padding(
-                padding: EdgeInsets.all(40.w),
+                padding: EdgeInsets.all(0.04.wp),
                 child: Text(
                   'Get your daily quote',
-                  style: TextStyle(fontSize: 36.sp),
+                  style: TextStyle(fontSize: 40.sp),
                   textAlign: TextAlign.center,
                 ),
               ),
